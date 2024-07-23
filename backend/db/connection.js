b22816cb790476fddb51aaadce734853d24855c9
Bryan <bryancui.111@gmail.com>
2024-07-23 11:10:37 -0400

    configured api

---
+++ backend/db/connection.js
@@ -0,0 +1,23 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected MongoDB");
+} catch (err) {
+    console.error(err);
+}
+
+let db = client.db("JojoConnections");
+
+export default db;
---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes/routes.js"
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/", routes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
