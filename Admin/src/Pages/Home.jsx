c0059b19d23742da1082dcc057470cfa091d0a0e
sp33dwag0n <bryancui.work@gmail.com>
2024-08-02 21:37:05 -0400

    begin styling

--- Admin/src/Pages/Home.jsx
+++ Admin/src/Pages/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../Styles/home.css';
 
 function Home() {
   const navigate = useNavigate();
   
   return (
     <div>
-        <h3>Home</h3>
-        <br />
-        <button onClick={() => navigate("/characters")}>Character List</button>
-        <button onClick={() => navigate("/catagories")}>Catagory List</button>
+        <div className="header">
+          <h3>Home</h3>
+        </div>
+        <div className="body">
+          <button className="btn" onClick={() => navigate("/characters")}>View Characters</button>
+          <button className="btn" onClick={() => navigate("/catagories")}>View Catagories</button>
+        </div>
     </div>
   )
 }
